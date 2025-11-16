8d8ae784dafe8cd0e47764f85717da5657fd3d71
SnepOMatic (Rhew) <github@rhew.co.uk>
2025-11-16 19:50:17 +0000

    Remove unused test.

--- src/filesystem/switch/SDL_sysfilesystem.c
+++ src/filesystem/switch/SDL_sysfilesystem.c
@@ -63,39 +63,6 @@ char *SDL_SYS_GetUserFolder(SDL_Folder folder)
     return NULL;
 }
 
-bool SDL_SYS_GetPathInfou(const char *path, SDL_PathInfo *info)
-{
-    SDL_PathInfo tmp_info;
-	struct stat statbuf;
-	const int rc = stat(path, &statbuf);
-
-	if (rc < 0) {
-		return SDL_SetError("Can't stat: %s", strerror(errno));
-	}
-	else if (S_ISREG(statbuf.st_mode)) {
-		tmp_info.type = SDL_PATHTYPE_FILE;
-		tmp_info.size = (Uint64) statbuf.st_size;
-	}
-	else if (S_ISDIR(statbuf.st_mode)) {
-		tmp_info.type = SDL_PATHTYPE_DIRECTORY;
-		tmp_info.size = 0;
-	}
-	else {
-		tmp_info.type = SDL_PATHTYPE_OTHER;
-		tmp_info.size = (Uint64) statbuf.st_size;
-	}
-
-	tmp_info.create_time = (SDL_Time) SDL_SECONDS_TO_NS(statbuf.st_ctime);
-	tmp_info.modify_time = (SDL_Time) SDL_SECONDS_TO_NS(statbuf.st_mtime);
-	tmp_info.access_time = (SDL_Time) SDL_SECONDS_TO_NS(statbuf.st_atime);
-
-	if (info) {
-		*info = tmp_info;
-	}
-
-	return true;
-}
-
 #endif /* SDL_FILESYSTEM_SWITCH */
 
 /* vi: set ts=4 sw=4 expandtab: */
